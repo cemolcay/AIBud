58fa20b9e066b82be3429fd25c508abf8ae8bcb0
cemolcay <ccemolcay@gmail.com>
2018-07-30 23:13:55 +0300

    Initial Commit

---
+++ CreateMLStuff/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CreateMLStuff
+//
+//  Created by Cem Olcay on 30.07.2018.
+//  Copyright © 2018 cemolcay. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
